8dbf49d999eab69e44bc2d3c5f766b19e263f41c
Nalam Mouneeswar <mouneeswarnalam18@gmail.com>
2026-06-11 16:07:15 +0530

    Switch to SQLite

--- paymenttracker/settings.py
+++ paymenttracker/settings.py
@@ -78,9 +78,10 @@ WSGI_APPLICATION = 'paymenttracker.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
