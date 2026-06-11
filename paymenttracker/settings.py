003ca79de6c5c03535d1bf905fef5132f5c82dbf
Nalam Mouneeswar <mouneeswarnalam18@gmail.com>
2026-06-11 15:19:41 +0530

    Deployment configuration

--- paymenttracker/settings.py
+++ paymenttracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$p9fu@(tj!$8lp#!ec6)jo5leho=2v!ymvshg&tnp$r^dbhy1)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
